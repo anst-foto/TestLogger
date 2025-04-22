e1bdc4f9b0990eae41053af53e9cdd9997168ff3
Starinin Andrey <starinin-andrey@ya.ru>
2025-04-22 11:45:02 +0300

    Init

---
+++ Calc.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Logger;
+
+namespace TestLogger;
+
+public class Calc
+{
+    private readonly ILogger _logger;
+
+    public Calc(ILogger logger)
+    {
+        _logger = logger;
+    }
+
+    public double Add(double a, double b)
+    {
+        var result = a + b;
+        _logger.LogInfo($"{a} + {b} = {result}");
+        return result;
+    }
+
+    public double Sub(double a, double b)
+    {
+        var result = a - b;
+        _logger.LogInfo($"{a} - {b} = {result}");
+        return result;
+    }
+    
+    public double Mul(double a, double b)
+    {
+        var result = a * b;
+        _logger.LogInfo($"{a} * {b} = {result}");
+        return result;
+    }
+    
+    public double Div(double a, double b)
+    {
+        if (b == 0)
+        {
+            _logger.LogError("Деление на ноль!");
+            throw new DivideByZeroException();
+        }
+        
+        var result = a / b;
+        _logger.LogInfo($"{a} / {b} = {result}");
+        return result;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Logger;
+using TestLogger;
+
+var calc = new Calc(new FileLogger("calc.log"));
+try
+{
+    calc.Add(1, 2);
+    calc.Sub(1, 2);
+    calc.Mul(1, 2);
+    calc.Div(1, 2);
+    calc.Div(1, 0);
+}
+catch (Exception e)
+{
+    // ignore
+}
